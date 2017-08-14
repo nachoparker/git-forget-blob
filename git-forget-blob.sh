c70469ed6039239872fd583f3db8f34669ac4f03
nachoparker <nacho@ownyourbits.com>
2017-08-14 16:48:10 +0200

    properly remove references to remotes. Use git gc

--- git-forget-blob.sh
+++ git-forget-blob.sh
@@ -14,7 +14,7 @@ function git-forget-blob()
   test -d .git || { echo "Need to be at base of a git repository" && return 1; }
   git repack -Aq
   ls .git/objects/pack/*.idx &>/dev/null || {
-    echo "there is nothing to be forgotten in this repo" && return;
+    echo "there is nothing to be forgotten in this repo" && return; 
   }
   local BLOBS=( $( git verify-pack -v .git/objects/pack/*.idx | grep blob | awk '{ print $1 }' ) )
   for ref in ${BLOBS[@]}; do
@@ -25,13 +25,14 @@ function git-forget-blob()
   [[ "$FILE" == "" ]] && { echo "$1 not found in repo history" && return; }
 
   git tag | xargs git tag -d
+  git branch -a | grep "remotes\/" | awk '{ print $1 }' | cut -f2 -d/ | while read r; do git remote rm $r 2>/dev/null; done
   git filter-branch --index-filter "git rm --cached --ignore-unmatch $FILE"
   rm -rf .git/refs/original/ .git/refs/remotes/ .git/*_HEAD .git/logs/
   git for-each-ref --format="%(refname)" refs/original/ | \
     xargs -n1 --no-run-if-empty git update-ref -d
   git reflog expire --expire-unreachable=now --all
   git repack -q -A -d
-  git prune
+  git gc --aggressive --prune=now
 }
 # License
 #
